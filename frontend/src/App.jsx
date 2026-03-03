4488f80e1d84d8416f41395b889746a62ca37820
Chengen_Hsieh <wayne70813@gmail.com>
2026-03-03 13:42:21 +0800

    refactor: move the Header and NavigationBar outside the Page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import WeatherPage from "./components/modules/weather/weather-page";
 import CurrencyPage from "./components/modules/currency/currency-page";
+import Header from "./components/common/header";
+import NavigationBar from "./components/common/navigation-bar";
 
 function App() {
   return (
     <div className="app-container">
-      {/* <WeatherPage></WeatherPage> */}
-      <CurrencyPage></CurrencyPage>
+      <Header pageTitle="匯率查詢"></Header>
+      <WeatherPage></WeatherPage>
+      {/* <CurrencyPage></CurrencyPage> */}
+      <NavigationBar></NavigationBar>
     </div>
   );
 }

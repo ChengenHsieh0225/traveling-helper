07d2e9dcc2b48671d160e6bb35411c298b8df46d
Chengen_Hsieh <wayne70813@gmail.com>
2026-03-03 15:30:14 +0800

    feat: implement routing using react router 1. Implement the routing between different pages. 2. Update the header and navigation bar based on the current page. 3. Implement a EmptyPage component.

--- frontend/src/components/common/header/index.jsx
+++ frontend/src/components/common/header/index.jsx
@@ -1,11 +1,22 @@
+import { useLocation } from "react-router-dom";
+
 import styles from "./style.module.css";
 import IconButton from "../buttons/icon-button";
 
-function Header({ pageTitle, isLoggedIn, isLightMode }) {
+function Header({ isLoggedIn, isLightMode }) {
+
+  const location = useLocation();
+
+  const pageTitles = {
+    '/currency': '匯率查詢',
+    '/weather': '天氣查詢',
+    '/news': '近期新聞',
+    '/locked': '此頁尚未開放'
+  }
 
   return (
     <div className={styles.headerContainer}>
-      <p className="body-bold">{pageTitle}</p>
+      <p className="body-bold">{pageTitles[location.pathname]}</p>
       <div className={styles.btnContainer}>
         <IconButton
           height="20px"
--- frontend/src/components/common/navigation-bar/index.jsx
+++ frontend/src/components/common/navigation-bar/index.jsx
@@ -1,40 +1,48 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 import styles from "./style.module.css";
 import IconButton from "../buttons/icon-button";
 
 function NavigationBar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div className={styles.outerContainer}>
-      <div className={styles.navigationPillContainer}>
+      <div className={styles.navigationPillContainer} onClick={() => navigate('./currency')}>
         <IconButton
           height="32px"
+          isActive={location.pathname === '/currency'}
           borderStyle="round"
           imgSrcActive="/assets/currency-filled.svg"
           imgSrcInactive="/assets/currency-outlined.svg"
         ></IconButton>
         <p className="caption">匯率</p>
       </div>
-      <div className={styles.navigationPillContainer}>
+      <div className={styles.navigationPillContainer} onClick={() => navigate('./weather')}>
         <IconButton
           height="32px"
-          isActive="true"
+          isActive={location.pathname === '/weather'}
           borderStyle="round"
           imgSrcActive="/assets/cloud-filled.svg"
           imgSrcInactive="/assets/cloud-outlined.svg"
         ></IconButton>
         <p className="caption">天氣</p>
       </div>
-      <div className={styles.navigationPillContainer}>
+      <div className={styles.navigationPillContainer} onClick={() => navigate('./news')}>
         <IconButton
           height="32px"
+          isActive={location.pathname === '/news'}
           borderStyle="round"
           imgSrcActive="/assets/news-filled.svg"
           imgSrcInactive="/assets/news-outlined.svg"
         ></IconButton>
         <p className="caption">新聞</p>
       </div>
-      <div className={styles.navigationPillContainer}>
+      <div className={styles.navigationPillContainer} onClick={() => navigate('./locked')}>
         <IconButton
           height="32px"
+          isActive={location.pathname === '/locked'}
           borderStyle="round"
           imgSrcActive="/assets/lock-filled.svg"
           imgSrcInactive="/assets/lock-outlined.svg"

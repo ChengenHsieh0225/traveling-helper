f74e6c2210b7a1196a6099c0456fc2246b808bd1
Chengen_Hsieh <wayne70813@gmail.com>
2026-03-04 15:53:09 +0800

    refactor: move the 'onClick' event handling for navigation into the IconButton

--- frontend/src/components/common/navigation-bar/index.jsx
+++ frontend/src/components/common/navigation-bar/index.jsx
@@ -9,43 +9,47 @@ function NavigationBar() {
 
   return (
     <div className={styles.outerContainer}>
-      <div className={styles.navigationPillContainer} onClick={() => navigate('./currency')}>
+      <div className={styles.navigationPillContainer}>
         <IconButton
           height="32px"
           isActive={location.pathname === '/currency'}
           borderStyle="round"
           imgSrcActive="/assets/currency-filled.svg"
           imgSrcInactive="/assets/currency-outlined.svg"
+          onClick={() => navigate('./currency')}
         ></IconButton>
         <p className="caption">匯率</p>
       </div>
-      <div className={styles.navigationPillContainer} onClick={() => navigate('./weather')}>
+      <div className={styles.navigationPillContainer}>
         <IconButton
           height="32px"
           isActive={location.pathname === '/weather'}
           borderStyle="round"
           imgSrcActive="/assets/cloud-filled.svg"
           imgSrcInactive="/assets/cloud-outlined.svg"
+          onClick={() => navigate('./weather')}
         ></IconButton>
         <p className="caption">天氣</p>
       </div>
-      <div className={styles.navigationPillContainer} onClick={() => navigate('./news')}>
+      <div className={styles.navigationPillContainer}>
         <IconButton
           height="32px"
           isActive={location.pathname === '/news'}
           borderStyle="round"
           imgSrcActive="/assets/news-filled.svg"
           imgSrcInactive="/assets/news-outlined.svg"
+          onClick={() => navigate('./news')}
         ></IconButton>
         <p className="caption">新聞</p>
       </div>
-      <div className={styles.navigationPillContainer} onClick={() => navigate('./locked')}>
+      <div className={styles.navigationPillContainer}>
         <IconButton
           height="32px"
           isActive={location.pathname === '/locked'}
           borderStyle="round"
           imgSrcActive="/assets/lock-filled.svg"
           imgSrcInactive="/assets/lock-outlined.svg"
+          onClick={() => navigate('./locked')}
         ></IconButton>
         <p className="caption">尚未開放</p>
       </div>

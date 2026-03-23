d29ab15ce5b66226e3b93dfc5c3862da31de0c12
Chengen_Hsieh <wayne70813@gmail.com>
2026-03-23 22:37:39 +0800

    feat: initialize the basic structure for the spring boot services 1. Rename the original backend folder as 'backend-python'. 2. Setup for spring boot, with the multi-modules structure.

---
+++ backend-java/auth-service/src/main/java/com/travelinghelper/auth/AuthApplication.java
@@ -0,0 +1,12 @@
+package com.travelinghelper.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}

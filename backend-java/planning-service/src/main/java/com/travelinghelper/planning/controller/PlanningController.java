dc38d7662abc0e7b8f16d5f5537b1e98c2c2e03f
Chengen_Hsieh <wayne70813@gmail.com>
2026-03-30 18:13:58 +0800

    feat: integrate OAuth2 Resource Server and JWT support to planning-service - Add the required dependency to pom.xml. - Configure application.yml with JWT public key location for token validation. - Implement SecurityConfig to protect API endpoints and enable stateless session. - Add a test endpoint in PlanningController to verify @AuthenticationPrincipal injection.

--- backend-java/planning-service/src/main/java/com/travelinghelper/planning/controller/PlanningController.java
+++ backend-java/planning-service/src/main/java/com/travelinghelper/planning/controller/PlanningController.java
@@ -6,8 +6,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,13 +19,12 @@ public class PlanningController {
 
     @PostMapping
     public ResponseEntity<String> createPlan(
-        @Valid @RequestBody CreatePlanRequest request
-//        @AuthenticationPrincipal Jwt jwt // Spring would extract the Bearer Token for Header automatically
+        @Valid @RequestBody CreatePlanRequest request,
+        @AuthenticationPrincipal Jwt jwt // Spring would extract the Bearer Token for Header automatically
     ) {
         // align with the jwt token generation logic within auth-service
-//        String userId = jwt.getSubject();
+        String userId = jwt.getSubject();
 
-        String userId = "waynehsieh";
         String planId = planningService.createPlan(request, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(planId);
     }
@@ -35,4 +34,11 @@ public class PlanningController {
         return "Hello!";
     }
 
+    @GetMapping("/userId")
+    public String jwtTest(
+        @AuthenticationPrincipal Jwt jwt
+    ) {
+        String userId = jwt.getSubject();
+        return "Your ID: " + userId;
+    }
 }

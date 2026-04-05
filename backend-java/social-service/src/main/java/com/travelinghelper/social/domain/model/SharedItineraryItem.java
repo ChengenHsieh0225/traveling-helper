883c00fcf0a2038ba68748bf53fd45e4744e0f1f
Chengen_Hsieh <wayne70813@gmail.com>
2026-04-05 18:08:20 +0800

    refactor(social-service): rename SharedItineraryItem table for semantic consistency

--- backend-java/social-service/src/main/java/com/travelinghelper/social/domain/model/SharedItineraryItem.java
+++ backend-java/social-service/src/main/java/com/travelinghelper/social/domain/model/SharedItineraryItem.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "social_itinerary_items")
+@Table(name = "shared_itinerary_items")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SharedItineraryItem {

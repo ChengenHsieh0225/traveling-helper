1977a4a5588291f10aff6b50fac1597aa35bef2e
Chengen_Hsieh <wayne70813@gmail.com>
2026-05-09 17:30:34 +0800

    feat(infra): initialize Terraform for AWS VPC setup - Configure AWS provider and commit dependency lock file (.terraform.lock.hcl). - Implement VPC using terraform-aws-modules with public/private subnets. - Enable NAT Gateway for private subnet internet access. - Update .gitignore to exclude Terraform state and local sensitive files.

---
+++ terraform/environments/prod/vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "traveling-helper-vpc"
+  cidr = "10.0.0.0/16"
+
+  # Available zones & Subnets
+  azs = ["ap-east-2a", "ap-east-2b", "ap-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # NAT Gateway
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
